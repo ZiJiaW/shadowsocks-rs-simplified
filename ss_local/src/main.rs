f5e311296dff9f1ccb3dca940790078e65377eca
ZiJiaW <964513305@qq.com>
2019-03-03 23:43:50 +0800

    handle connection 50%

--- ss_local/src/main.rs
+++ ss_local/src/main.rs
@@ -11,7 +11,7 @@ use futures::future::{self, Either};
 use bytes::{BytesMut, Bytes, BufMut};
 
 use std::collections::HashMap;
-use std::net::SocketAddr;
+use std::net::{SocketAddr, Ipv4Addr, IpAddr};
 use std::sync::{Arc, Mutex};
 
 const SS_SERVER_ADDR: &'static str = "127.0.0.1:9002";
@@ -32,30 +32,35 @@ fn main()
 }
 
 mod Socks5 {
-    pub const VER: u8 = 0x5;
-    pub const AUTH: u8 = 0x0;
+    pub const VER: u8 = 0x05;// protocol version
+    pub const AUTH: u8 = 0x00;// auth type: no auth
+    pub const CMD_TCP: u8 = 0x01;// tcp connection proxy
+    pub const CMD_UDP: u8 = 0x03;// udp request
+    pub const ATYP_V4: u8 = 0x01;// ipv4 address type
+    pub const ATYP_DN: u8 = 0x03;// domain name address type
+    pub const REP_OK: u8 = 0x00;
 }
 
+enum RQ_ADDR {
+    IPV4(SocketAddr),
+    STR(String),
+}
+
+// hand shake part of socks5 protocol
 fn hand_shake(socket: TcpStream) -> impl Future<Item = TcpStream, Error = io::Error>
 {
-    io::read_exact(socket, vec![0u8])
+    io::read_exact(socket, vec![0u8; 2])
     .and_then(|(socket, buf)| {
         if buf[0] != Socks5::VER {
             println!("addr {:?} socks version is {}!", socket.peer_addr().unwrap(), buf[0]);
             return Err(io::Error::new(io::ErrorKind::InvalidData, "Wrong version!"));
         } else {
-            Ok(socket)
+            let len = buf[1];
+            Ok((socket, len))
         }
     })
-    .and_then(|socket| {
-        io::read_exact(socket, vec![0u8])
-        .and_then(|(socket, buf)| {
-            let len = buf[0];
-            Ok((socket, len))
-        })
-    })
     .and_then(|(socket, len)| {
-        io::read_exact(socket, vec![0u8, len])
+        io::read_exact(socket, vec![0u8; len as usize])
         .and_then(|(socket, buf)| {
             match buf.iter().find(|&&x| x == Socks5::AUTH) {
                 Some(_) => Ok(socket),
@@ -71,12 +76,60 @@ fn hand_shake(socket: TcpStream) -> impl Future<Item = TcpStream, Error = io::Er
     })
 }
 
-//fn connect()
+// connection configure part
+fn handle_connect(socket: TcpStream) -> impl Future<Item = (TcpStream, RQ_ADDR), Error = io::Error>
+{
+    io::read_exact(socket, vec![0u8; 4])
+    .and_then(|(socket, buf)| {
+        if buf[0] != Socks5::VER {
+            Err(io::Error::new(io::ErrorKind::InvalidData, "Wrong version!"))
+        } else if buf[1] != Socks5::CMD_TCP {
+            Err(io::Error::new(io::ErrorKind::PermissionDenied, "No UDP support!"))
+        } else {
+            if buf[3] == Socks5::ATYP_V4 {
+                //Either::A(
+                    io::read_exact(socket, vec![0u8; 6])
+                    .and_then(|(socket, buf)| {
+                        let addr = Ipv4Addr::new(buf[0], buf[1], buf[2], buf[3]);
+                        let port = ((buf[4] as u16) << 8) | (buf[5] as u16);
+                        let addr = RQ_ADDR::IPV4(SocketAddr::new(IpAddr::V4(addr), port));
+                        Ok((socket, addr))
+                    })
+                //)
+            } else {
+                //Either::B(
+                    io::read_exact(socket, vec![0u8])
+                    .and_then(|(socket, buf)| {
+                        let len = buf[0];
+                        io::read_exact(socket, vec![0u8; len as usize])
+                        .and_then(|(socket, buf)| {
+                            let addr = RQ_ADDR::STR(String::from_utf8(buf).unwrap());
+                            Ok((socket, addr))
+                        })
+                    })
+                //)
+            }
+        }    
+    })
+    .and_then(|(socket, addr)| {
+        let response: Vec<u8> = vec![
+            Socks5::VER, Socks5::REP_OK, 0x0, Socks5::ATYP_V4,
+            0x0, 0x0, 0x0, 0x0, 0x10, 0x10
+        ];
+        io::write_all(socket, response)
+        .and_then(|(socket, _)| {
+            Ok((socket, addr))
+        })
+    })
+}
 
 // Main processing logic
 fn process(socket: TcpStream)
 {
     let handler = hand_shake(socket)
+    .and_then(|socket| {
+        handle_connect(socket)
+    })
     .map(|_|{})
     .map_err(|_|{});
     tokio::spawn(handler);
